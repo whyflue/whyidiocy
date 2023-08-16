821423ce29d35a9217828794ffe1078f3d611cdb
whynot <123102331+whyflue@users.noreply.github.com>
2023-08-16 17:53:09 +0300

    adding registration form pt.1

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import "./Register.scss"
 
 export const Register = () => {
   return (
-    <div>Register</div>
+    <div className="formContainer">
+        <div className="formWrapper">
+            <span className="logo">WhySocium</span>
+            <span className="title">Registration</span>
+            <form>
+                <input type="text" placeholder="display name"/>
+                <input type="email" placeholder="email"/>
+                <input type="password" placeholder="password"/>
+                <input type="file"/>
+                <button>Sign Up!</button>
+            </form>
+            <p>Already have an account? Log in</p>
+        </div>
+    </div>
   )
 }
